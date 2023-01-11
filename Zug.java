d6391bf114f9f782521112d583f96b006d80f69c
Robin Krestel <87312441+verfxgbar@users.noreply.github.com>
2023-01-11 10:54:05 +0100

    Created Zug.java

---
+++ Zug.java
@@ -0,0 +1,9 @@
+public class Zug {
+	private String zugName = "";
+	private int zugNummer;
+	
+	public Zug(String zugName, int zugNummer) {
+		this.zugName = zugName;
+		this.zugNummer = zugNummer;
+	}
+}
